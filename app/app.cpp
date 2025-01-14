a05f2636d6c4ce04814ef6b2357db6d8efbad735
Tomislav Prstec <toprstec@vsite.hr>
2025-01-14 21:00:50 +0100

    v09 solved

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,74 @@
 
 namespace vsite::oop::v9
 {
+	void fill_vector(std::vector<int>& v, int size) 
+	{
+		for (int i = 0; i < size; ++i) {
+			v.push_back(i * i);
+		}
+	}
+
+	void remove_element(std::vector<int>& v, int index) 
+	{
+		v.erase(v.begin() + index);
+	}
+
+	void input_element(std::vector<std::string>& v, int index, std::string s)
+	{
+		v.insert(v.begin() + index, s);
+	}
+	
+	int list_nth_element(std::list<int>& l, int index)
+	{
+		auto it = l.begin();
+		for (int i = 0; i < index; ++i) {
+			++it;
+		}
+		return *it;
+	}
+
+	void list_sort_desc(std::list<int>& l)
+	{
+		l.sort(std::greater<int>());
+	}
+
+	unsigned unique_numbers(std::istream& is) 
+	{
+		std::set<int> s{};
+		int num;
+		while (is >> num) {
+			s.insert(num);
+		}
+		return s.size();
+	}
+
+	word_frequency::word_frequency(std::istream& is) 
+	{
+		std::string word;
+		while (is >> word) {
+			word[0] = tolower(word[0]);
+			insert(word);
+		}
+	}
+
+	void word_frequency::insert(const std::string& s)
+	{
+		++m[s];
+	}
+
+	unsigned word_frequency::count() const
+	{
+		return m.size();
+	}
+
+	unsigned word_frequency::frequency(std::string s) const
+	{
+		auto it = m.find(s);
+		return it != m.end() ? it->second : 0;
+	}
+
+
+
+
 
 }
--- app/app.h
+++ app/app.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+#include <string>
+
 
 namespace vsite::oop::v9 
 {
+	void fill_vector(std::vector<int>& v, int size);
+	void remove_element(std::vector<int>& v, int index);
+	void input_element(std::vector<std::string>& v, int index, std::string s);
+	int list_nth_element(std::list<int>& l, int index);
+	void list_sort_desc(std::list<int>& l);
+	unsigned unique_numbers(std::istream& is);
+
+	class word_frequency {
+
+		std::map<std::string, unsigned> m;
+		void insert(const std::string& s);
+
+	public:
+
+		word_frequency(std::istream& is);
+		unsigned count()const;
+		unsigned frequency(std::string s)const;
+
+	};
 }
